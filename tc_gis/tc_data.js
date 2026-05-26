ec8e9d12ea2a94f2ce90de5648beb3d527f82ca4
macaosso <tungtung0191@gmail.com>
2026-05-26 18:32:09 +0800

    Update tc_data.js

--- tc_gis/tc_data.js
+++ tc_gis/tc_data.js
@@ -1,21 +1,18 @@
 const forecastInfo = {
-  updated: "05-26 04:00 MST"
+  updated: "05-26 18:00 MST"
 };
 
 const pastPoints = [
-  {lat:6, lon:145, time:"06-08 00H", wind: 40},
-  {lat:7, lon:142, time:"06-09 00H", wind: 45},
-  {lat:8, lon:140, time:"06-10 00H", wind: 55},
-  {lat:9, lon:139, time:"06-11 00H", wind: 65},
-  {lat:10, lon:137, time:"06-12 00H", wind: 85}
+  {lat:8.1, lon:137.3, time:"05-26 18H", wind: 40},
+  
 ];
 
 const forecastPoints = [
-  {lat:10, lon:137, time:"06-12 00H", wind: 85},
-  {lat:11, lon:133, time:"06-12 12H", wind: 110},
-  {lat:13, lon:130, time:"06-13 00H", wind: 130},
-  {lat:15, lon:123, time:"06-14 00H", wind: 160},
-  {lat:17, lon:120, time:"06-15 00H", wind: 190},
-  {lat:19, lon:117, time:"06-16 00H", wind: 175},
-  {lat:20, lon:113, time:"06-17 00H", wind: 140}
+  {lat:8.1, lon:137.3, time:"06-26 18H", wind: 40},
+  {lat:8.4, lon:137.8, time:"06-27 06H", wind: 50},
+  {lat:9.6, lon:136.7, time:"06-27 18H", wind: 55},
+  {lat:13.5, lon:134.4, time:"06-28 18H", wind: 75},
+  {lat:15.7, lon:131.9, time:"06-29 18H", wind: 105},
+  {lat:17.5, lon:129.4, time:"06-30 18H", wind: 145},
+  {lat:21.3, lon:128.0, time:"07-01 18H", wind: 165}
 ];
