2a27097e69436858b66c97da59bfa735ce5d0d41
macaosso <tungtung0191@gmail.com>
2026-05-27 15:31:54 +0800

    Update tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -1,19 +1,21 @@
 const forecastInfo = {
-  updated: "05-27 04:00 MST"
+  updated: "05-27 15:00 MST"
 };
 
 const pastPoints = [
-  {lat:8.1, lon:137.3, time:"05-26 18H", wind: 45},
+  {lat:8.1, lon:137.3, time:"05-26 20H", wind: 45},
   {lat:8.5, lon:137.5, time:"05-27 02H", wind: 55},
+  {lat:9.1, lon:137.6, time:"05-27 08H", wind: 55},
+  {lat:9.6, lon:137.3, time:"05-27 14H", wind: 65},
   
 ];
 
 const forecastPoints = [
-  {lat:8.5, lon:137.5, time:"05-27 02H", wind: 55},
-  {lat:9.9, lon:137.1, time:"05-27 14H", wind: 65},
-  {lat:11.4, lon:136.6, time:"05-28 02H", wind: 75},
-  {lat:14.1, lon:134.8, time:"05-29 02H", wind: 105},
-  {lat:16.4, lon:132.1, time:"05-30 02H", wind: 130},
-  {lat:19.7, lon:130.8, time:"05-31 02H", wind: 155},
-  {lat:23.8, lon:130.1, time:"06-01 02H", wind: 175}
+  {lat:9.6, lon:137.3, time:"05-27 14H", wind: 65},
+  {lat:11.6, lon:136.7, time:"05-28 02H", wind: 75},
+  {lat:13.0, lon:136.0, time:"05-28 14H", wind: 85},
+  {lat:15.7, lon:134.1, time:"05-29 14H", wind: 110},
+  {lat:18.3, lon:131.4, time:"05-30 14H", wind: 140},
+  {lat:22.2, lon:130.5, time:"05-31 14H", wind: 175},
+  {lat:25.9, lon:131.0, time:"06-01 14H", wind: 185}
 ];
