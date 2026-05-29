1a9511d79b8840b114442fe8b3168e8365835dfa
macaosso <tungtung0191@gmail.com>
2026-05-29 14:18:20 +0800

    Update tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -14,20 +14,20 @@ const pastPoints = [
   {lat:13.8, lon:135.5, time: "05-28 20H", wind: 75},  
   {lat:14.3, lon:134.9, time: "05-29 02H", wind: 75},  
   {lat:15.2, lon:134.0, time: "05-29 08H", wind: 85},  
-  {lat:15.5, lon:132.6, time: "05-29 11H", wind: 85},  
+  {lat:15.7, lon:133.0, time: "05-29 14H", wind: 85},  
 
 ];
 
 const forecastPoints = [
-  {lat:15.5, lon:132.6, time: "05-29 11H", wind: 85,
+  {lat:15.7, lon:133.0, time: "05-29 11H", wind: 85,
    
     wind41: { ne: 220, se: 240, sw: 180, nw: 180 }, 
     wind88: { ne: 0, se: 0, sw: 0,  nw: 0 } 
    
   },
 
-  {lat:16.4, lon:131.3, time:"05-29 23H", wind: 105},
-  {lat:17.2, lon:130.1, time:"05-30 11H", wind: 130},
+  {lat:16.8, lon:131.2, time:"05-30 02H", wind: 105},
+  {lat:17.9, lon:129.7, time:"05-30 14H", wind: 130},
   {lat:19.4, lon:128.5, time:"05-31 11H", wind: 155},
   {lat:24.2, lon:126.2, time:"06-01 11H", wind: 175},
   {lat:29.0, lon:128.8, time:"06-02 11H", wind: 130},
