e955bc12a7d2652a03fe09d5bff0842a445866e1
macaosso <tungtung0191@gmail.com>
2026-05-30 04:52:23 +0800

    Update warn.js

--- warn.js
+++ warn.js
@@ -1,10 +1,12 @@
 const warnWarnings = [
   {
     icon: "",
-    text: "非常酷熱警報於本地時間05月29日19時20分取消。"
+    text: ""
   },
   {
-    icon: "warn_icon/r0.jpg",
-    text: "降雨預警於本地時間05月29日21時40分發出，有效時間至23時00分。"
+    icon: "",
+    text: ""
   }
 ];
+
+//warn_icon/r0.jpg
