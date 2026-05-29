d14be0e20d50d0f9d394848682c35990ac613820
macaosso <tungtung0191@gmail.com>
2026-05-29 23:58:43 +0800

    Update tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -1,6 +1,6 @@
 const tcWarnings = [
   {
-    text: "「薔薇」已經增強成為強烈熱帶風暴，晚上10時，「薔薇」集結在本澳東南偏東約2000公里，即北緯16.4度，東經131.7度，其中心附近最高持續風速約為每小時90公里，向西北移動，時速20公里，未來數日會逐漸增強，大致移向台灣以東海域至沖繩一帶。"
+    text: "「薔薇」已經增強成為強烈熱帶風暴，晚上11時，「薔薇」集結在本澳東南偏東約2000公里，即北緯16.4度，東經131.7度，其中心附近最高持續風速約為每小時90公里，向西北移動，時速20公里，未來數日會逐漸增強，大致移向台灣以東海域至沖繩一帶。"
   },
 ];
 
@@ -8,7 +8,7 @@ const tcWarnings = [
 //icon:
 
 const forecastInfo = {
-  updated: "05-29 22:00 MST"
+  updated: "05-29 23:00 MST"
 };
 
 const pastPoints = [
@@ -25,23 +25,23 @@ const pastPoints = [
   {lat:15.2, lon:134.0, time: "05-29 08H", wind: 85},  
   {lat:15.6, lon:132.8, time: "05-29 14H", wind: 85},  
   {lat:16.2, lon:132.0, time: "05-29 20H", wind: 90}, 
-  {lat:16.4, lon:131.7, time: "05-29 22H", wind: 90}, 
+  {lat:16.6, lon:131.8, time: "05-29 23H", wind: 90}, 
 ];
 
 const forecastPoints = [
-  {lat:16.4, lon:131.7, time: "05-29 22H",wind: 90,
+  {lat:16.4, lon:131.7, time: "05-29 23H",wind: 90,
    
     wind41: { ne: 220, se: 260, sw: 200, nw: 180 }, 
     wind88: { ne: 60, se: 100, sw: 60,  nw: 40 } 
    
   },
 
-  {lat:17.8, lon:129.8, time:"05-30 10H", wind: 120},
-  {lat:18.9, lon:128.5, time:"05-30 22H", wind: 155},
-  {lat:22.3, lon:126.7, time:"05-31 22H", wind: 175},
-  {lat:26.4, lon:126.4, time:"06-01 22H", wind: 140},
-  {lat:31.2, lon:132.1, time:"06-02 22H", wind: 120},
-  {lat:35.5, lon:141.4, time:"06-03 22H", wind: 90},
+  {lat:17.8, lon:129.8, time:"05-30 11H", wind: 120},
+  {lat:18.9, lon:128.5, time:"05-30 23H", wind: 140},
+  {lat:23.3, lon:126.7, time:"05-31 23H", wind: 165},
+  {lat:26.4, lon:126.4, time:"06-01 23H", wind: 155},
+  {lat:31.2, lon:132.1, time:"06-02 23H", wind: 130},
+  {lat:35.5, lon:141.4, time:"06-03 23H", wind: 90},
 ];
 
 window.tcInfo = {
