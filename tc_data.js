6fdf3ecac774222264920a386daef45ff1ec6463
macaosso <tungtung0191@gmail.com>
2026-05-30 12:19:45 +0800

    Update tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -1,6 +1,6 @@
 const tcWarnings = [
   {
-    text: "「薔薇」已經增強成為強烈熱帶風暴，晚上11時，「薔薇」集結在本澳東南偏東約2010公里，即北緯16.6度，東經131.8度，其中心附近最高持續風速約為每小時90公里，向西北移動，時速21公里，未來數日會逐漸增強，大致移向台灣以東海域至沖繩一帶。"
+    text: "正午12時，強烈熱帶風暴「薔薇」集結在本澳東南偏東約2010公里，即北緯16.6度，東經131.8度，其中心附近最高持續風速約為每小時90公里，向西北移動，時速21公里，未來數日會逐漸增強，大致移向台灣以東海域至沖繩一帶。"
   },
 ];
 
@@ -8,7 +8,7 @@ const tcWarnings = [
 //icon:
 
 const forecastInfo = {
-  updated: "05-29 23:00 MST"
+  updated: "05-30 12:00 MST"
 };
 
 const pastPoints = [
@@ -25,29 +25,28 @@ const pastPoints = [
   {lat:15.2, lon:134.0, time: "05-29 08H", wind: 85},  
   {lat:15.6, lon:132.8, time: "05-29 14H", wind: 85},  
   {lat:16.2, lon:132.0, time: "05-29 20H", wind: 90}, 
-  {lat:16.6, lon:131.8, time: "05-29 23H", wind: 90}, 
+  {lat:16.8, lon:131.0, time: "05-30 02H", wind: 90}, 
+  {lat:17.3, lon:130.2, time: "05-30 08H", wind: 105}, 
+  {lat:17.9, lon:129.8, time: "05-30 12H", wind: 105}, 
 ];
 
 const forecastPoints = [
-  {lat:16.6, lon:131.8, time: "05-29 23H",wind: 90,
+  {lat:17.9, lon:129.8, time: "05-30 12H",wind: 105,
    
-    wind41: { ne: 220, se: 260, sw: 200, nw: 180 }, 
-    wind88: { ne: 60, se: 100, sw: 60,  nw: 40 } 
-   
+    wind41: { ne: 230, se: 270, sw: 220, nw: 200 }, 
+    wind88: { ne: 80, se: 120, sw: 80,  nw: 60 } 
   },
 
-  {lat:17.7, lon:130.0, time:"05-30 11H", wind: 120},
-  {lat:19.6, lon:128.3, time:"05-30 23H", wind: 140},
-  {lat:22.9, lon:127.1, time:"05-31 23H", wind: 165},
-  {lat:27.1, lon:128.1, time:"06-01 23H", wind: 155},
-  {lat:32.1, lon:134.5, time:"06-02 23H", wind: 130},
-  {lat:35.7, lon:143.1, time:"06-03 23H", wind: 90},
+  {lat:19.4, lon:128.7, time:"05-31 00H", wind: 120},
+  {lat:21.2, lon:128.0, time:"05-30 12H", wind: 130},
+  {lat:25.6, lon:127.6, time:"05-31 12H", wind: 155},
+  {lat:30.0, lon:130.9, time:"06-01 12H", wind: 145},
+  {lat:34.2, lon:138.3, time:"06-02 12H", wind: 105},
+  {lat:36.5, lon:146.8, time:"06-03 12H", wind: "-"},
 ];
 
 window.tcInfo = {
   name: "薔薇 (2606)", 
   wind: forecastPoints[0].wind,
-  pressure: 985
+  pressure: 980
 };
-
- 
