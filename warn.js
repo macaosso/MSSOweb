15d6f5cd42d1cdcc3fe3b74a4d56c27845fdaa9c
macaosso <tungtung0191@gmail.com>
2026-05-31 06:15:11 +0800

    Update warn.js

--- warn.js
+++ warn.js
@@ -1,7 +1,8 @@
 const warnWarnings = [
   {
-    icon: "",
-    text: "酷熱警報於本地時間05月31日05時30分取消。"
+    icon: "warn_icon/h1.jpg",
+    text: "酷熱警報於本地時間05月30日12時00分發出，預料本澳今日天氣酷熱，最高氣溫約33 - 34度。
+"
   },
   {
     icon: "warn_icon/r1.jpg",
