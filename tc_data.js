a9ae10ba7cbb141272842ceaf9026e6f01b5d7c2
macaosso <tungtung0191@gmail.com>
2026-06-04 14:06:21 +0800

    更新 tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -12,42 +12,14 @@ const forecastInfo = {
 };
 
 const pastPoints = [
-  {lat:8.1, lon:137.3, time:"05-26 20H", wind: 45},
-  {lat:8.5, lon:137.5, time:"05-27 02H", wind: 45},
-  {lat:9.1, lon:137.6, time:"05-27 08H", wind: 50},
-  {lat:9.6, lon:137.5, time:"05-27 14H", wind: 50},
-  {lat:10.2, lon:137.3, time:"05-27 20H", wind: 55},
-  {lat:11.3, lon:136.5, time: "05-28 02H", wind: 55},
-  {lat:12.4, lon:136.3, time: "05-28 08H", wind: 65},  
-  {lat:13.5, lon:136.0, time: "05-28 14H", wind: 65},
-  {lat:13.8, lon:135.5, time: "05-28 20H", wind: 75},  
-  {lat:14.3, lon:134.9, time: "05-29 02H", wind: 75},  
-  {lat:15.2, lon:134.0, time: "05-29 08H", wind: 85},  
-  {lat:15.6, lon:132.8, time: "05-29 14H", wind: 85},  
-  {lat:16.2, lon:132.0, time: "05-29 20H", wind: 90}, 
-  {lat:16.8, lon:131.0, time: "05-30 02H", wind: 105}, 
-  {lat:17.3, lon:130.2, time: "05-30 08H", wind: 120}, 
-  {lat:18.2, lon:129.5, time: "05-30 14H", wind: 120}, 
-  {lat:19.1, lon:129.0, time: "05-30 20H", wind: 130}, 
-  {lat:20.0, lon:128.4, time: "05-31 02H", wind: 130}, 
-  {lat:20.8, lon:128.2, time: "05-31 08H", wind: 140}, 
-  {lat:21.8, lon:127.9, time: "05-31 14H", wind: 140},  
-  {lat:22.7, lon:127.7, time: "05-31 20H", wind: 130},
-  {lat:23.6, lon:127.4, time: "06-01 02H", wind: 130},
-  {lat:24.4, lon:127.3, time: "06-01 08H", wind: 120},
-  {lat:25.4, lon:127.4, time: "06-01 14H", wind: 110},
-  {lat:26.5, lon:127.7, time: "06-01 20H", wind: 110},
-  {lat:27.8, lon:128.3, time: "06-02 02H", wind: 105},
-  {lat:28.9, lon:128.9, time: "06-02 08H", wind: 105},
-  {lat:30.3, lon:130.5, time: "06-02 14H", wind: 90},
-  {lat:32.3, lon:133.0, time: "06-02 20H", wind: 90},
+  {lat:19.0, lon:118.3, time:"06-04 14H", wind: 45},
 ];
 
 const forecastPoints = [
-  {lat:32.3, lon:133.0, time: "06-02 20H", wind: 90,
-   
-    wind41: { ne: 230, se: 270, sw: 220, nw: 200 }, 
-    wind88: { ne: 80, se: 120, sw: 80,  nw: 60 } 
+  {lat:19.0, lon:118.3, time: "06-04 14H", wind: 90,
+  
+    wind41: { ne: 0, se: 0, sw: 0, nw: 0 }, 
+    wind88: { ne: 0, se: 0, sw: 0,  nw: 0 } 
   },
   { lat: 33.6, lon: 136, time: "06-03 08H", wind: 90 },  
   { lat: 35.2, lon: 140.7, time: "06-03 20H", wind: 85 },
@@ -57,7 +29,7 @@ const forecastPoints = [
 ];
 
 window.tcInfo = {
-  name: "薔薇 (2606)", 
+  name: "低壓區 (91W)", 
   wind: forecastPoints[0].wind,
-  pressure: 985
+  pressure: 1006
 };
