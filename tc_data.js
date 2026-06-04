b07aa1e697a1ed94263bafb792650c920c9e1334
macaosso <tungtung0191@gmail.com>
2026-06-04 18:45:13 +0800

    更新 tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -8,21 +8,22 @@ const tcWarnings = [
 //icon:
 
 const forecastInfo = {
-  updated: "06-04 14:00 MST"
+  updated: "06-04 17:00 MST"
 };
 
 const pastPoints = [
   {lat:19.0, lon:118.3, time:"06-04 14H", wind: 40},
+  {lat:19.5, lon:118.6, time:"06-04 17H", wind: 45},
 ];
 
 const forecastPoints = [
-  {lat:19.0, lon:118.3, time: "06-04 14H", wind: 40,
+  {lat:19.5, lon:118.6, time: "06-04 17H", wind: 45,
   
     wind41: { ne: 0, se: 0, sw: 0, nw: 0 }, 
     wind88: { ne: 0, se: 0, sw: 0,  nw: 0 } 
   },
-  { lat: 20.6, lon: 119.2, time: "06-05 02H", wind: 40 },  
-  { lat: 22.4, lon: 121.1, time: "06-05 14H", wind: 45 },
+  { lat: 20.6, lon: 119.2, time: "06-05 02H", wind: 50 },  
+  { lat: 22.4, lon: 121.1, time: "06-05 14H", wind: 55 },
   { lat: 25.6, lon: 124.2, time: "06-06 14H", wind: 65 },
   { lat: 31.1, lon: 131.2, time: "06-07 14H", wind: 75 },
   { lat: 34.8, lon: 140.4, time: "06-08 14H", wind: 65 },
@@ -33,5 +34,5 @@ const forecastPoints = [
 window.tcInfo = {
   name: " 91W", 
   wind: forecastPoints[0].wind,
-  pressure: 1006
+  pressure: 1004
 };
