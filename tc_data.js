80b93796a950e25e8cb81171a73480b41f02d572
macaosso <tungtung0191@gmail.com>
2026-06-04 14:11:31 +0800

    更新 tc_data.js

--- tc_data.js
+++ tc_data.js
@@ -12,19 +12,17 @@ const forecastInfo = {
 };
 
 const pastPoints = [
-  {lat:19.0, lon:118.3, time:"06-04 14H", wind: 45},
+  {lat:19.0, lon:118.3, time:"06-04 14H", wind: 40},
 ];
 
 const forecastPoints = [
-  {lat:19.0, lon:118.3, time: "06-04 14H", wind: 90,
+  {lat:19.0, lon:118.3, time: "06-04 14H", wind: 40,
   
     wind41: { ne: 0, se: 0, sw: 0, nw: 0 }, 
     wind88: { ne: 0, se: 0, sw: 0,  nw: 0 } 
   },
-  { lat: 33.6, lon: 136, time: "06-03 08H", wind: 90 },  
-  { lat: 35.2, lon: 140.7, time: "06-03 20H", wind: 85 },
-  { lat: 36.7, lon: 146.6, time: "06-04 20H", wind: 85 },
-  { lat: 36.4, lon: 149.8, time: "06-05 20H", wind: "-" }
+  { lat: 20.6, lon: 119.2, time: "06-05 02H", wind: 40 },  
+  { lat: 22.4, lon: 121.1, time: "06-05 14H", wind: 40 }
 
 ];
 
