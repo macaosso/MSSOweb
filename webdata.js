dec5f53e3fd4cedbdce5bdd8980aca6eb26b21be
macaosso <tungtung0191@gmail.com>
2026-06-06 12:00:21 +0800

    更新 webdata.js

--- webdata.js
+++ webdata.js
@@ -9,8 +9,8 @@ const tcWarnings = [
 //Warning
 const warnWarnings = [
   {
-    icon: "warn_icon/h1.jpg",
-    text: "酷熱警報於本地時間06月05日16時20分發出，取代非常酷熱警報。預料本澳明日部分地區天氣酷熱，最高氣溫約33度。"
+    icon: "",
+    text: ""
   },
   {
     icon: "",
@@ -22,7 +22,7 @@ const warnWarnings = [
 const swtText = ``;
 
 //Alert Tips
-const alertipsText = `高溫預警提示於本地時間05月23日19時20分發出。表示澳門高溫天氣持續。（2026-06-04 14:20 MST 更新)`;
+const alertipsText = ``;
 
 //Weather Forecast
 const forecastRaw = `隨着西南氣流逐漸加強，週末期間間中有驟雨及雷暴。一股弱冷空氣將會在下週初影響本澳，與西南季候風匯聚形成輻合帶，珠江口一帶下週初至中期雨勢加強，有頻密狂風大驟雨及猛烈雷暴，週二天氣較為不穩定，雨勢頗大，可能有豪雨。週三至週四受冷空氣影響，鋒面雨帶被推至南海，週五至下週末期間冷空氣減弱，西南氣流重新主導，鋒面雨帶推至珠江口沿岸，該區雨勢重新增強，雨勢頗大及有狂風雷暴。
