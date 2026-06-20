7b77f96c66a98f8da7beb4a666252424e269babb
macaosso <tungtung0191@gmail.com>
2026-06-20 23:29:45 +0800

    更新 webdata.js

--- webdata.js
+++ webdata.js
@@ -9,8 +9,8 @@ const tcWarnings = [
 //Warning
 const warnWarnings = [
   {
-    icon: "",
-    text: ""
+    icon: "icons/h1.jpg",
+    text: "酷熱警報於本地時間06月20日08時20分發出，預料本澳今日天氣酷熱，最高氣溫約33-34度。"
   },
   {
     icon: "",
